bc26bb5d08526a5eb665580d206540607db1814c
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-14 21:50:15 -0500

    Crear clase rectangulo

---
+++ Ejemplo_01/Rectangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_01
+{
+    class Rectangulo
+    {
+        public Punto punto1 { get; set; }
+        public Punto punto2 { get; set; }
+        public Punto punto3 { get; set; }
+        public Punto punto4 { get; set; }
+        
+        public double CalcularArea()
+        {
+            double base_,altura;
+            base_ = CalcularBase();
+            altura = CalcularAltura();
+            return base_ * altura;
+        }
+
+        private double CalcularBase()
+        {            
+            return 1;
+        }
+        private double CalcularAltura()
+        {
+            return 1;
+        }
+
+    }
+}
